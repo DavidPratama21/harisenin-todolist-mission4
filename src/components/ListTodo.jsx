060ad601243c1d52e1221f19639eb9fa78a283da
Daun2121 <rayruby210203@gmail.com>
2025-05-15 16:34:26 +0700

    pusing

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -12,30 +12,59 @@ const ListTodo = () => {
   return (
     <div className='p-2 grid w-full rounded-lg bg-secondary shadow-[0_1px_4px_rgba(13,71,161,.25)]'>
       {/* Title & Pick todo to delete */}
-      <div className='flex'>
-        {/* Title & pick todo */}
-        <div className='flex w-full gap-1 items-center'>
-          {/* Pick todo */}
-          {/* <button onClick={handleClick}>
-            {click ? <Square strokeWidth={1} /> : <SquareCheck strokeWidth={1} />}
-          </button> */}
-          <h3 className='font-bold'>Todo</h3>
-          <h3 className='font-bold'>Priority</h3>
-          <h3 className='font-bold'>Due date</h3>
-          
-        </div>
-
+      <div className='grid items-center grid-cols-[auto_2fr_1fr_1fr_auto] gap-1 text-[20px]'>
+        {/* Pick todo */}
+        <Square className='col-auto'/>
+        <h3 className='font-bold'>Todo</h3>
+        <h3 className='font-bold'>Priority</h3>
+        <h3 className='font-bold'>Due date</h3>
         {/* Delete BTN */}
         <Trash2 color='#757575'/>
       </div>
 
+
       {/* List todo */}
       <div className='grid gap-1 w-full text-sm'>
-        <Todo task="Study react.js" bgPrio={'high'} priority={'High'} dueDate={'25 Mei 2025'}/>
+        <Todo task="Study react.js lalu mandi makan tidur" bgPrio={'high'} priority={'High'} dueDate={'25 Mei 2025'}/>
         <Todo task="Ngepel kamar" bgPrio={'medium'} priority={'Medium'} dueDate={'27 Mei 2025'}/>
         <Todo task="Mancing" bgPrio={'low'} priority={'Low'} dueDate={'1 Januari 2027'}/>
       </div>
     </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // <div className='p-2 grid w-full rounded-lg bg-secondary shadow-[0_1px_4px_rgba(13,71,161,.25)]'>
+    //   {/* Title & Pick todo to delete */}
+    //   <div className='flex'>
+    //     {/* Title & pick todo */}
+    //     <div className='flex w-full gap-1 items-center'>
+    //       {/* Pick todo */}
+    //       {/* <button onClick={handleClick}>
+    //         {click ? <Square strokeWidth={1} /> : <SquareCheck strokeWidth={1} />}
+    //       </button> */}
+    //       <h3 className='font-bold'>Todo</h3>
+    //       <h3 className='font-bold'>Priority</h3>
+    //       <h3 className='font-bold'>Due date</h3>
+          
+    //     </div>
+
+    //     {/* Delete BTN */}
+    //     <Trash2 color='#757575'/>
+    //   </div>
+
+    //   {/* List todo */}
+    //   <div className='grid gap-1 w-full text-sm'>
+    //     <Todo task="Study react.js" bgPrio={'high'} priority={'High'} dueDate={'25 Mei 2025'}/>
+    //     <Todo task="Ngepel kamar" bgPrio={'medium'} priority={'Medium'} dueDate={'27 Mei 2025'}/>
+    //     <Todo task="Mancing" bgPrio={'low'} priority={'Low'} dueDate={'1 Januari 2027'}/>
+    //   </div>
+    // </div>
   )
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,54 @@
 import React from 'react'
 import {Square, SquareCheck, X} from 'lucide-react';
 
+// const [done, setDone] = useState(false);
+
+
+
 export const Todo = ({task, bgPrio = 'done' , priority, dueDate, done}) => {
   return (
-    <div className={`flex justify-between items-start text-${done}`}>
-      {/* Todo */}
-      <div className='flex gap-1 items-start'>
-        {/* Done Btn */}
-        <button><Square strokeWidth={1} color='#757575'/></button>
-        {/* Todo, Priority, Due date */}
-        <div className='grid'>
-          <p>{task}</p>
-          {/* Priority & Due date */}
-          <div className='flex gap-1'>
-            {/* Priority */}
-            <span className={`p-0.5 text-${bgPrio} bg-${bgPrio}/25 rounded font-bold`}>{priority}</span>
-            {/* <span className='p-0.5 text-high bg-high/25 rounded font-bold'>High</span> */}
-            {/* Due date */}
-            <span className='p-0.5 text-done bg-done/25 rounded'>{dueDate}</span>
-          </div>
-        </div>
-      </div>
+    <div className={`grid lg:items-start text-${done} grid-cols-[auto_2fr_1fr_1fr_auto] gap-1`}>
+      {/* Done Btn */}
+      <button><Square strokeWidth={1} color='#757575'/></button>
+      <p className={`${done ? 'line-through' : ''} self-start`}>{task}</p>
+      {/* Priority */}
+      <span className={`rounded font-bold py-1 px-2 w-fit text-${bgPrio} bg-${bgPrio}/25`}>{priority}</span>
+      {/* Due date */}
+      <span className='p-0.5 rounded w-fit text-nowrap'>{dueDate}</span>
       {/* Delete */}
       <button><X color='#757575'/></button>
     </div>
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    // <div className={`flex justify-between items-start text-${done}`}>
+    //   {/* Todo */}
+    //   <div className='flex gap-1 items-start'>
+    //     {/* Done Btn */}
+    //     <button><Square strokeWidth={1} color='#757575'/></button>
+    //     {/* Todo, Priority, Due date */}
+    //     <div className='grid lg:flex'>
+    //       <p>{task}</p>
+    //       {/* Priority & Due date */}
+    //       <div className='flex gap-1'>
+    //         {/* Priority */}
+    //         <span className={`p-0.5 text-${bgPrio} bg-${bgPrio}/25 rounded font-bold`}>{priority}</span>
+    //         {/* <span className='p-0.5 text-high bg-high/25 rounded font-bold'>High</span> */}
+    //         {/* Due date */}
+    //         <span className='p-0.5 text-done bg-done/25 rounded'>{dueDate}</span>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* Delete */}
+    //   <button><X color='#757575'/></button>
+    // </div>
   )
 }
 
